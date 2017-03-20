1b6d5ccc147d4e3e4e3ac4c1f88ac884d649628a
deepankar0522 <deepravi0522@gmail.com>
2017-03-20 18:06:00 +0530

    register working

--- FirstProject/mySQLquery.sql
+++ FirstProject/mySQLquery.sql
@@ -10,5 +10,6 @@ alter table UserLogin auto_increment=10000;
 insert into UserLogin(userid, email, password) values('100000','deepankar@gmail.com','deepankar');
 describe UserLogin;
 select * from UserLogin;
+Select count(1) from UserLogin where email = 'deepankar@gmail.com' and password = 'deepankar';
 
-                        
+insert into userlogin(email, password) values('ravi@gmail.com','ravi');
